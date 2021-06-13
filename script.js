6e7990e63211974ccdf398cbd99fc9400f6dacbd
Rajendra <nathrajen2710@gmail.com>
2021-06-13 20:54:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let menuBtn=document.querySelector(".menu-btn");
+let cancelBtn=document.querySelector(".cancel-btn");
+let navBar=document.querySelector(".navbar");
+let body = document.querySelector("body");
+
+menuBtn.onclick=function(){
+    menuBtn.style.opacity="0";
+    menuBtn.style.pointerEvent="none";
+    navBar.classList.add("active");
+    body.style.overflow="hidden";
+}
+cancelBtn.onclick=function(){
+    menuBtn.style.opacity="1";
+    menuBtn.style.pointerEvent="auto";
+    navBar.classList.remove("active");
+    body.style.overflow="auto";
+}
+
+//Sticky Navigation Menu JS code
+let nav=document.querySelector("nav");
+let val;
+window.onscroll=function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+}
+
+let navLinks = document.querySelectorAll(".menu li a");
+for(var i=0;i<navLinks.length;i++){
+    navLinks[i].addEventListener("click",()=>{
+        menuBtn.style.opacity="1";
+    menuBtn.style.pointerEvent="auto";
+    navBar.classList.remove("active");
+    body.style.overflow="auto";
+
+    });
+
+}
